1c73131f356775fb7a5946129c3a61da7260be44
VinciDaKinG <kamil.pajak75@wp.pl>
2019-05-20 16:59:34 +0200

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+
+            Console.WriteLine("Robert to pala");
+
+            return 0;
+        }
+
+
+    }
+}
+
+
